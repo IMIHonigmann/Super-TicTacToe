1fbc5d447c2f97c2f997788b6b601471c3bda2d9
Homam Mousa <homammousa15@gmail.com>
2024-06-26 18:54:16 +0200

    rooms limit themselves to 2 clients now

--- server/index.js
+++ server/index.js
@@ -10,18 +10,25 @@ const io = require('socket.io')(server, {
 
 io.on('connection', (socket) => {
     socket.on('joinRoom', (data, cb) => {
-        console.log('User joined in ' + data);
-        socket.join(data);
-        cb(`You have joined the room: ${data}`);
-        io.to(data).emit('logmessage', `A new user has joined`);
+        const roomSize = io.sockets.adapter.rooms.get(data)?.size || 0;
+        if(roomSize < 2) {
+            console.log('User joined in ' + data);
+            socket.join(data);
+            socket.to(data).emit('logmessage', `A new user has joined`);
+            cb(`You have successfully joined the room: ${data}`);
+        }
+        else {
+            cb(`You cannot join because room: ${data} is full`);
+        }
     });
     
     socket.on('message', (message, room) => {
         io.to(room).emit('message', message, room);
     });
 
-    socket.on('sendChoice', (data, room, numberAsString, clickedValidField) => {
-        socket.to(room).emit('getChoice', data, room, numberAsString, clickedValidField);
+    socket.on('sendChoice', (socketID, data, room, numberAsString, clickedValidField) => {
+        const roomIdentifier = io.sockets.adapter.rooms.get(room);
+        if(roomIdentifier.has(socketID)) socket.to(room).emit('getChoice', data, room, numberAsString, clickedValidField);
         // socket.broadcast.emit('getChoice', data, room, numberAsString, clickedValidField);
     });
 });
